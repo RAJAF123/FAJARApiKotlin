c3f230b3ecfd1e1c5e0b2057bb121770b64a75d9
RAJAF123 <92904410+RAJAF123@users.noreply.github.com>
2021-10-28 10:27:58 +0700

    aplikasikotlin
    membuat aplikasi sederhana

---
+++ app/src/main/java/com/example/apikotlin/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.apikotlin;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
--- app/src/main/java/com/example/apikotlin/MainActivity.kt
+++ app/src/main/java/com/example/apikotlin/MainActivity.kt
@@ -1,11 +1,11 @@
-package com.example.apikotlin
-
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
-import java.nio.file.attribute.AclFileAttributeView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.apikotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +13,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
     fun submit_btn(view: View) {
-        val namaEditText=findViewById<EditText>(R.id.nama_edit_text)
+        val namaEditText = findViewById<EditText>(R.id.nama_edit_text)
         val nama = namaEditText.text.toString()
-
-        val toast= Toast.makeText(applicationContext,nama,Toast.LENGTH_SHORT)
+        val toast = Toast.makeText(applicationContext, nama, Toast.LENGTH_SHORT)
         toast.show()
+        AlertDialog.Builder(this)
+            .setIcon(R.mipmap.ic_launcher)
+            .setTitle("Hai Cantik")
+            .setMessage("I Love You")
+            .setPositiveButton("OK",
+                DialogInterface.OnClickListener { dialog, which ->
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Kamu memilih OK",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                })
+            .setNegativeButton("Cancel",
+                DialogInterface.OnClickListener { dialog, which ->
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Kamu memilih Cancel",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                })
+            .show()
+    }
+}
